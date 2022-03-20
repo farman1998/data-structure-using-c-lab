537bde39af067cc73f9ae95dc7cb8da7505f46ee
MOHD FARMAN <101541144+farman1998@users.noreply.github.com>
2022-03-20 14:04:13 +0530

    Add files via upload
    find the area of rectangle ..while taking input from user

---
+++ area of rectangle.c.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int height;
+    int width;
+    printf("enter heigth of the rectangle: \n");
+    scanf("%d",&height);
+    printf("enter width of the rectangle: \n");
+    scanf("%d",&width);
+                    printf("Area of rectangle %d:",height*width);  
+    return 0;
+    
+}
